81518112c48df1dd802f236d7850f6d65fa7a1df
Kiran607 <ickirankiru@gmail.com>
2020-06-22 16:47:04 +0530

    [Kiran]Added condtion example program

---
+++ ConditionAndLoop/Condtion.js
@@ -0,0 +1,25 @@
+number=(num)=>{
+    if(num==4){
+        console.log("Hello");
+    }
+    else{
+        console.log("Wrong input");
+    }
+}
+
+number(5);
+
+//else-if
+exampleElseIf=(num)=>{
+    if(num==4){
+        console.log("Hello");
+    }
+    else if(num==5){
+        console.log("Hello world");
+    }
+    else{
+        console.log("Hello world hi")
+    }
+}
+
+exampleElseIf(4);
