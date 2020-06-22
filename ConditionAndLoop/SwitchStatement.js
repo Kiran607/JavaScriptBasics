8472d045dcb59c7a88c84257cc098b4893485200
Kiran607 <ickirankiru@gmail.com>
2020-06-22 17:11:36 +0530

    [Kiran]Added switch case program concept

---
+++ ConditionAndLoop/SwitchStatement.js
@@ -0,0 +1,14 @@
+let a=2+2;
+switch(a){
+    case 1:
+        console.log("Value is wrong");
+        break;
+    case 2:
+        console.log("value is wrong");
+        break;
+    case 4:
+        console.log("Value matched");
+        break;
+    default:
+        console.log("Value did not match");
+}
