f35ad3d0d4343764c2329a69aa86804d5b4347fa
Kiran607 <ickirankiru@gmail.com>
2020-06-22 17:34:43 +0530

    [Kiran]Added while loop program

---
+++ ConditionAndLoop/WhileLoop.js
@@ -0,0 +1,15 @@
+//While statememnt
+let n = 0;
+let x = 0;
+while (n < 3) {
+  n++;
+  x += n;
+  console.log(x);
+}
+
+//Do while statement
+let i = 0;
+do {
+  i += 1;
+  console.log(i);
+} while (i < 5);
