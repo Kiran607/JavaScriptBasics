d836c4392c4257212bcb45cf3885ef8a00fb0532
Kiran607 <ickirankiru@gmail.com>
2020-06-23 08:54:44 +0530

    [Kiran\Added forloop program

---
+++ ConditionAndLoop/ForLoop.js
@@ -0,0 +1,12 @@
+for (let step = 0; step < 5; step++) {
+    // Runs 5 times, with values of step 0 through 4.
+    console.log('Walking east one step',step);
+  }
+
+  //for each
+  let fruits=["Apple","Mango","JackFruit"];
+  fruits.forEach(element=>console.log(element));
+ 
+  for(let i of fruits){
+      console.log(i)
+  }
